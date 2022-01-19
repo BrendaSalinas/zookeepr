ccfc5e09a447b33079670036e389541a7c0214cd
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-01-19 06:57:23 -0600

    changing 3001 to PORT

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 //start express.js app 
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 var app = express()
 const { animals } =require('./data/animals.json');
+
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -53,7 +55,7 @@ app.get('/api/animals', (req, res) => {
 });
 
 //call out the port 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
 
