8276bbf812482b8946c9233683aa59a3f48a737b
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-01-22 12:36:52 -0600

    working on POST

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/api/animals/:id', (req, res) => {
 });
 
 //get routes that we have created
-app.post('api/animals', (req, res) => {
+app.post('/api/animals', (req, res) => {
     //req body is where our incoming content will be
     console.log(req.body);
     res.json(req.body);
